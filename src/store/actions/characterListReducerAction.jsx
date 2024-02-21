16807b4b2e8485372de685ce1457b4597f9f9296
Kerem Karaman <keremkaraman91@gmail.com>
2024-02-21 20:03:43 +0300

    .env bug fixed

--- src/store/actions/characterListReducerAction.jsx
+++ src/store/actions/characterListReducerAction.jsx
@@ -1,6 +1,21 @@
+import axios from "axios";
 export const FETCH_HEROES = "FETCH_HEROES";
+
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+console.log(BASE_URL);
 
 export const setHeroes = (data) => ({
   type: FETCH_HEROES,
   payload: data,
 });
+
+export const fetchRoles = () => () => {
+  axios
+    .get("roles")
+    .then((res) => {
+      dispatch(setHeroes(res.data));
+    })
+    .catch((err) => {
+      console.error("Error fetching roles: ", err);
+    });
+};

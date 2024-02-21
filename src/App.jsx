620f1d8b0673ecba5012ff0713306ed3f4431e52
Kerem Karaman <keremkaraman91@gmail.com>
2024-02-21 18:11:02 +0300

    react router added to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+//Pages
+import HeroList from "./components/HeroList";
+import HeroDetail from "./components/HeroDetail";
+import ErrorPage from "./components/ErrorPage";
 function App() {
   return (
-    <div>
-      <h1>hello marve asdasdl</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<HeroList />} />
+      <Route path="/herodetail:id" element={<HeroDetail />} />
+      <Route path="*" element={<ErrorPage />} />
+    </Routes>
   );
 }
 
